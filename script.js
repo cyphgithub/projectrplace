8c8d3c2563124a8fb109f34472f5d85a9239d08a
cyph <89238641+cyphgithub@users.noreply.github.com>
2022-04-02 01:31:45 -0500

    cum mit

--- script.js
+++
@@ -1,7 +0,0 @@
-function playaudio() {
-    var audio = new Audio('nevergonnagiveyouup.mp3');
-    console.log("working..");
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    console.log("worked");
-    audio.play();
-  }
